52e5e1e1a041f121ceb22a36d013c55c05b32f07
Oliwka202 <oliwia.radzikowska@student.wat.edu.pl>
2026-06-03 13:44:58 +0200

    complete library management module

--- controller.py
+++ controller.py
@@ -1,13 +1,20 @@
 from model import biblioteki
+import requests
+import folium
+from bs4 import BeautifulSoup
 
 
 def add_biblioteka():
     nazwa = input("Podaj nazwę biblioteki: ")
-    miasto = input("Podaj miasto: ")
+    miasto = input("Podaj miasto: ").title()
+
+    coordinates = get_coordinates(miasto)
 
     biblioteki.append({
         "nazwa": nazwa,
-        "miasto": miasto
+        "miasto": miasto,
+        "lat": coordinates[0],
+        "lon": coordinates[1]
     })
 
     print("Biblioteka została dodana")
@@ -36,10 +43,15 @@ def update_biblioteka():
     if 1 <= numer <= len(biblioteki):
 
         nowa_nazwa = input("Nowa nazwa: ")
-        nowe_miasto = input("Nowe miasto: ")
+        nowe_miasto = input("Nowe miasto: ").title()
 
         biblioteki[numer - 1]["nazwa"] = nowa_nazwa
         biblioteki[numer - 1]["miasto"] = nowe_miasto
+
+        nowe_wspolrzedne = get_coordinates(nowe_miasto)
+
+        biblioteki[numer - 1]["lat"] = nowe_wspolrzedne[0]
+        biblioteki[numer - 1]["lon"] = nowe_wspolrzedne[1]
 
         print("Biblioteka została zaktualizowana")
 
@@ -65,3 +77,45 @@ def delete_biblioteka():
 
     else:
         print("Nieprawidłowy numer")
+
+
+def get_coordinates(miasto):
+
+    url = f'https://pl.wikipedia.org/wiki/{miasto}'
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    response_html = BeautifulSoup(response.text, 'html.parser')
+
+    latitude = float(
+        response_html.select('.latitude')[1].text.replace(',', '.')
+    )
+
+    longitude = float(
+        response_html.select('.longitude')[1].text.replace(',', '.')
+    )
+
+    return [latitude, longitude]
+
+
+def get_mapa():
+
+    m = folium.Map([52, 21], zoom_start=6)
+
+    for biblioteka in biblioteki:
+
+        folium.Marker(
+            location=[
+                biblioteka['lat'],
+                biblioteka['lon']
+            ],
+            popup=biblioteka['nazwa']
+        ).add_to(m)
+
+    m.save("mapa_bibliotek.html")
+
+    print("Mapa została zapisana")
